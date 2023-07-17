478879f34fdcbb4fd8013ca90d567cdff3e18fb5
MatheusVict <matheusvictorhenrique@gmail.com>
2023-07-17 20:26:28 -0300

    colors and viewbinding

--- app/src/main/java/dev/matheusvict/nubankinterface/MainActivity.kt
+++ app/src/main/java/dev/matheusvict/nubankinterface/MainActivity.kt
@@ -2,10 +2,13 @@ package dev.matheusvict.nubankinterface
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dev.matheusvict.nubankinterface.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
